0b80755a03519bcf3af0e7f571609cd97adbc452
ivand <ivan.dolgushin@lucidrealitylabs.com>
2019-12-27 00:40:24 +0200

    Basic script posting a fresh build to a chosen slack channel

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # unity-utils-publish-builds
 A simple Editor tool to automatically publish new Unity builds to Slack channel
+
+# Usage
+Put slack.json file into your Unity project root:
+```json
+{
+	"oauthToken": "your OAuth token for Slack app with 'files:write:user' and 'remote_files:share' permissions", 
+	"uploadEndpoint": "https://slack.com/api/files.upload", 
+	"channel": "channel id you want to post your build to"
+}
+```
+You can add this file to your .gitignore to keep your OAuth token safely out of repository.
+
+# Dependencies
+- [unity-utils-editor-coroutine](https://github.com/traleven/unity-utils-editor-coroutine) makes possible to use UnityWebRequest in Editor.
+- [SharpZipLib](https://github.com/icsharpcode/SharpZipLib) packs the build into zip archive.
---
+++ SlackPost.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public static class SlackPost
+{
+	[System.Serializable]
+	public struct SlackCredentials
+	{
+		public string oauthToken;
+		public string uploadEndpoint;
+		public string channel;
+	}
+
+	[PostProcessBuild(1010)]
+	public static void PostToSlack(BuildTarget target, string pathToBuiltProject)
+	{
+		SlackCredentials credentials = JsonUtility.FromJson<SlackCredentials>(File.ReadAllText("slack.json"));
+
+		string version = PlayerSettings.bundleVersion;
+		string buildGuid = GUID.Generate().ToString().Substring(0, 6);
+		string pathToBuild = Path.GetDirectoryName(pathToBuiltProject);
+		string dir = Path.GetDirectoryName(pathToBuild);
+		string zipName = Path.Combine(dir, Path.GetFileName(pathToBuild) + ".zip");
+
+		if (File.Exists(zipName))
+		{
+			File.Delete(zipName);
+		}
+
+		var archive = new ICSharpCode.SharpZipLib.Zip.FastZip();
+		{
+			archive.CreateEmptyDirectories = true;
+			archive.CreateZip(zipName, pathToBuild, true, "");
+		}
+
+		EditorCoroutine.StartCoroutine(SendWebRequest(
+			credentials,
+			zipName,
+			version + "." + buildGuid,
+			$"New {target} build {version}"));
+	}
+
+	private static IEnumerator SendWebRequest(SlackCredentials credentials, string zipName, string version, string message)
+	{
+		UnityWebRequest uploadRequest = UnityWebRequest.Post(credentials.uploadEndpoint, new List<IMultipartFormSection> {
+			new MultipartFormFileSection("file", File.ReadAllBytes(zipName), $"@{zipName}", "application/zip"),
+			new MultipartFormDataSection("filename", $"{Path.GetFileNameWithoutExtension(zipName)}.{version}{Path.GetExtension(zipName)}"),
+			new MultipartFormDataSection("title", $"{Path.GetFileNameWithoutExtension(zipName)}.{version}"),
+			new MultipartFormDataSection("channels", credentials.channel),
+			new MultipartFormDataSection("initial_comment", message),
+		});
+
+		uploadRequest.SetRequestHeader("Authorization", $"Bearer {credentials.oauthToken}");
+
+
+		Debug.Log($"Uploading {zipName}");
+		uploadRequest.SendWebRequest();
+
+		while (!uploadRequest.isDone)
+			yield return null;
+
+		if (uploadRequest.isNetworkError)
+			Debug.Log(uploadRequest.error);
+		else
+			Debug.Log($"Posted to Slack\n{uploadRequest.downloadHandler.text}");
+	}
+
+	[MenuItem("Lucid/Post to Slack")]
+    public static void PostToSlack()
+	{
+		string filePath = Path.Combine("/Users/diplomat/Projects/Lucid/Nextmotion/NRS", "Builds", "NRS-Linux", "NRS-Main.x86_64");
+		PostToSlack(BuildTarget.StandaloneLinux64, filePath);
+	}
+}
